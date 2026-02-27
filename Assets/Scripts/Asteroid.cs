60aae45ec090131a65d9a4d018ffab3ebecb81e2
Tallie Tye <tallie.tye@outlook.com>
2026-02-27 14:10:09 +0000

    Add random sizing to Asteroids

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -5,7 +5,8 @@ public class Asteroid : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float randomSize = Random.Range(0.5f, 2.0f);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
     }
 
     // Update is called once per frame
